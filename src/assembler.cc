cd1e18bd7cd45f9740e297c6cff9042c10e47b0b
salesvictor <salesrvictor@gmail.com>
2018-11-13 09:30:42 -0200

    Changed formatting to Google C++ Style Guide

--- src/assembler.cc
+++ src/assembler.cc
@@ -1,129 +1,81 @@
 #include "assembler.h"
 
-namespace mws
-{
-  namespace assembler
-  {
-    std::string translate_token(std::string token)
-    {
-      // IMP
-      if (token == "SM")
-      {
-        return " ";
-      }
-      if (token == "AM")
-      {
-        return "\t ";
-      }
-      if (token == "HA")
-      {
-        return "\t\t";
-      }
-      if (token == "FC")
-      {
-        return "\n";
-      }
-      if (token == "IO")
-      {
-        return "\t\n";
-      }
-
-      // SM Instrunctions
-      if (token == "PUSH")
-      {
-        return " ";
-      }
-      if (token == "DUP")
-      {
-        return "\n ";
-      }
-      if (token == "SWP")
-      {
-        return "\n\t";
-      }
-      if (token == "POP")
-      {
-        return "\n\n";
-      }
-
-      // AM Instrunctions
-      if (token == "ADD")
-      {
-        return " ";
-      }
-      if (token == "SUB")
-      {
-        return "\n ";
-      }
-      if (token == "MUL")
-      {
-        return "\n\t";
-      }
-      if (token == "DIV")
-      {
-        return "\n\n";
-      }
-      if (token == "MOD")
-      {
-        return "\t\t";
-      }
-
-      // HA Instrunctions
-      if (token == "STR")
-      {
-        return " ";
-      }
-      if (token == "RTR")
-      {
-        return "\t";
-      }
-
-      // FC Instrunctions
-      if (token == "LABEL")
-      {
-        return " ";
-      }
-      if (token == "CALL")
-      {
-        return "\n ";
-      }
-      if (token == "JMP")
-      {
-        return "\n\t";
-      }
-      if (token == "BRNZ")
-      {
-        return "\n\n";
-      }
-      if (token == "RET")
-      {
-        return "\t\t";
-      }
-      if (token == "END")
-      {
-        return "\n\n";
-      }
-
-      // IO Instrunctions
-      if (token == "PUTC")
-      {
-        return "  ";
-      }
-      if (token == "PUTI")
-      {
-        return " \t";
-      }
-      if (token == "GETC")
-      {
-        return "\t ";
-      }
-      if (token == "GETI")
-      {
-        return "\t\t";
-      }
-
-      // Invalid Token
-      return "INVALID";
+namespace mws {
+namespace assembler {
+  std::string translate_token(std::string token) {
+    // IMP
+    if (token == "SM") {
+      return " ";
+    } else if (token == "AM") {
+      return "\t ";
+    } else if (token == "HA") {
+      return "\t\t";
+    } else if (token == "FC") {
+      return "\n";
+    } else if (token == "IO") {
+      return "\t\n";
     }
-  } // namespace assembler
+
+    // SM Instrunctions
+    if (token == "PUSH") {
+      return " ";
+    } else if (token == "DUP") {
+      return "\n ";
+    } else if (token == "SWP") {
+      return "\n\t";
+    } else if (token == "POP") {
+      return "\n\n";
+    }
+
+    // AM Instrunctions
+    if (token == "ADD")
+    {
+      return " ";
+    } else if (token == "SUB") {
+      return "\n ";
+    } else if (token == "MUL") {
+      return "\n\t";
+    } else if (token == "DIV") {
+      return "\n\n";
+    } else if (token == "MOD") {
+      return "\t\t";
+    }
+
+    // HA Instrunctions
+    if (token == "STR") {
+      return " ";
+    } else if (token == "RTR") {
+      return "\t";
+    }
+
+    // FC Instrunctions
+    if (token == "LABEL") {
+      return " ";
+    } else if (token == "CALL") {
+      return "\n ";
+    } else if (token == "JMP") {
+      return "\n\t";
+    } else if (token == "BRNZ") {
+      return "\n\n";
+    } else if (token == "RET") {
+      return "\t\t";
+    } else if (token == "END") {
+      return "\n\n";
+    }
+
+    // IO Instrunctions
+    if (token == "PUTC") {
+      return "  ";
+    } else if (token == "PUTI") {
+      return " \t";
+    } else if (token == "GETC") {
+      return "\t ";
+    } else if (token == "GETI") {
+      return "\t\t";
+    }
+
+    // Invalid Token
+    return "INVALID";
+  }
+} // namespace assembler
 } // namespace mws
--- src/assembler.h
+++ src/assembler.h
@@ -1,14 +1,12 @@
-#ifndef MWS_ASSEMBLER_H
-#define MWS_ASSEMBLER_H
+#ifndef MWS_ASSEMBLER_H_
+#define MWS_ASSEMBLER_H_
 
 #include <string>
 
-namespace mws
-{
-  namespace assembler
-  {
-    std::string translate_token(std::string token);
-  }
-}
+namespace mws {
+namespace assembler {
+  std::string translate_token(std::string token);
+} // namespace assembler
+} // namespace mws
 
-#endif // MWS_ASSEMBLER_H
+#endif // MWS_ASSEMBLER_H_
