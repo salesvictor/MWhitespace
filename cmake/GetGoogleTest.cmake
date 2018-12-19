2da4beafe4c589b22d109bd2b56e858f075840f9
salesvictor <salesrvictor@gmail.com>
2018-12-19 00:39:48 -0200

    Now it is (almost) as I wanted it to be

--- cmake/GetGoogleTest.cmake
+++ cmake/GetGoogleTest.cmake
@@ -2,13 +2,11 @@ set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 
 include(FetchContent)
 
-message(STATUS "Downloading googletest")
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG master
+  GIT_TAG release-1.8.1
 )
-message(STATUS "Downloading googletest - done")
 
 FetchContent_GetProperties(googletest)
 
@@ -16,16 +14,23 @@ if(NOT googletest_POPULATED)
   message(STATUS "Populating googletest")
   FetchContent_Populate(googletest)
   message(STATUS "Populating googletest - done")
-  add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR})
+  add_subdirectory(${googletest_SOURCE_DIR} ${googletest_BINARY_DIR}
+    EXCLUDE_FROM_ALL)
 endif()
 
 include(CTest)
+include(GoogleTest)
 
 macro(add_gtest TEST_NAME)
   add_executable(${TEST_NAME} ${TEST_NAME}.cc)
-  target_link_libraries(${TEST_NAME} gtest gtest_main gmock ${ARGN})
-  add_test(NAME ${TEST_NAME} COMMAND ${TEST_NAME} --gtest_color=yes)
-  set_target_properties(${TEST_NAME} PROPERTIES FOLDER test)
+  target_link_libraries(${TEST_NAME}
+    PUBLIC gtest gtest_main gmock ${ARGN})
+  gtest_discover_tests(
+    ${TEST_NAME}
+    EXTRA_ARGS --gtest_color=yes
+    TEST_PREFIX "${TEST_NAME}."
+    PROPERTIES FOLDER "test")
 endmacro()
 
-add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} --verbose)
+add_custom_target(check
+  COMMAND ${CMAKE_CTEST_COMMAND} --force-new-ctest-process --output-on-failure)
