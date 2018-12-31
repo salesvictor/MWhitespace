33181f309d5d9a62afe734bdc62177f693aa5319
salesvictor <salesrvictor@gmail.com>
2018-12-30 23:04:06 -0200

    Added a white line just because

--- mws/include/mws/assembler.h
+++ mws/include/mws/assembler.h
@@ -9,4 +9,5 @@ std::string SubstituteToken(const std::string &token);
 void AssembleProgram(std::istream &program, std::ostream &out_program);
 } // namespace assembler
 } // namespace mws
+
 #endif // MWS_INCLUDE_MWS_ASSEMBLER_H
